cad58ded6788b757d3ad8c7856621322ac0ecfe4
VIGNESH KUMAR <vigneshkumar3497@gmail.com>
2023-06-08 01:29:02 +0530

    first commit

---
+++ apidoc.md
@@ -0,0 +1,31 @@
+---------KFC Application-----------
+
+Mongodb - mongodb+srv://vicky_03:2gBDRqpEelizHMgw@kfc.uh3mvos.mongodb.net/?retryWrites=true&w=majority
+
+--------- page-1 -----------
+
+=> List of city 
+- http://localhost:4545/location
+
+
+--------- page-2 -----------
+
+=> all menu list 
+- http://localhost:4545/menu
+
+
+--------- page-3 -----------
+
+=> Details of Menu selected -
+
+=> Place Order -
+
+
+
+--------- page-4 -----------
+
+=> List of all the orders -
+
+=> Update orders details -
+
+=> Delete Orders -
---
+++ app.js
@@ -0,0 +1,117 @@
+let express = require('express');
+let app = express();
+let port = process.env.PORT||4545;
+let Mongo = require('mongodb');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+let {dbConnect,getData,postData,updateOrder,deleteOrder} = require('./controller/dbController')
+
+
+/// middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(cors())
+ 
+app.get('/',(req,res) => {
+    res.send('Hi from express')
+})
+
+/// all location 
+
+app.get('/location',async (req,res) => {
+    let query = {}
+    if(req.query.stateId){
+        query={state_id: Number(req.query.stateId)}
+    }
+    else{
+        query = {}
+    }
+    let collection = "location"
+    let output = await getData(collection,query) 
+    res.send(output)
+})
+
+
+/// all menutypes                   
+
+app.get('/menu', async (req,res) => {
+    let query = {};
+    if(req.query.menuId){
+        query={menu_id: Number(req.query.menuId)}
+    }
+    else{
+        query = {}
+    }
+    let collection = "menu";
+    let output = await getData(collection,query);
+    res.send(output)
+})
+
+/// orders
+
+app.get('/orders', async (req,res) => {
+    let query = {};
+    if(req.query.email){
+        query={email:req.query.email}
+    }
+    else{
+        query = {}
+    }
+    let collection = "orders";
+    let output = await getData(collection,query);
+    res.send(output)
+})
+
+/// placeOrders
+
+app.post('/placeOrder',async(req,res) => {
+    let data = req.body;
+    let collection = "orders";
+    let response = await postData(collection,data)
+    res.send(response)
+})
+
+
+//menu details
+
+app.post('/menuDetails',async(req,res) => {
+    if(Array.isArray(req.body.id)){
+        let query = {menu_id:{$in:req.body.id}};
+        let collection = 'menu';
+        let output = await getData(collection,query);
+        res.send(output)
+    }
+    else {
+        res.send('Please Pass data  in form of array')
+    }
+})
+
+
+/// update orders
+
+app.put('/updateOrder',async(req,res) => {
+    let collection = 'orders';
+    let condition = {"_id":new Mongo.ObjectId(req.body._id)}
+    let data = {
+        $set:{
+            "status":req.body.status
+        }
+    }
+    let output = await updateOrder(collection,condition,data)
+    res.send(output)
+})
+
+/// delete orders
+
+app.delete('/deleteOrder',async(req,res) => {
+    let collection = 'orders';
+    let condition = {"_id":new Mongo.ObjectId(req.body._id)}
+    let output = await deleteOrder(collection,condition)
+    res.send(output)
+})
+
+app.listen(port,(err) => {
+    dbConnect()
+    if(err) throw err;
+    console.log(`Server is running on port ${port}`)
+})
